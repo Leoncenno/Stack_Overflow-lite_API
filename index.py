19d90c1687deaaf30589b8079cb56bb0d28431ce
ongebo <isaacongebo@gmail.com>
2021-07-11 17:03:20 +0300

    Remove Interactions class

--- index.py
+++ index.py
@@ -1,4 +1,3 @@
-import flask
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -24,43 +23,41 @@ questions = [
 ]
 
 
-class Interactions():
-    @app.route('/api/v1/questions', methods=['GET'])
-    def all_questions():
-        return jsonify(questions)
+@app.route('/api/v1/questions', methods=['GET'])
+def all_questions():
+    return jsonify(questions)
 
-    @app.route('/api/v1/questions/id', methods=['GET'])
-    def one_question():
-        if 'id' in request.args:
-            id = int(request.args['id'])
+
+@app.route('/api/v1/questions/id', methods=['GET'])
+def one_question():
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return"Sorry, no match"
+    search = []
+    for question in questions:
+        if question['id'] == id:
+            search.append(question)
+    return jsonify(search)
+
+
+@app.route('/api/v1/questions', methods=['POST'])
+def add_question():
+    added_question = questions.append(request.get_json())
+    return jsonify(added_question)
+
+
+@app.route('/api/v1/questions/id/answers', methods=['POST'])
+def add_answer():
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    answers = []
+    for question in questions:
+        if question['id'] == id:
+            added_answer = answers.append(request.get_json())
+            return jsonify(answers)
         else:
-            return"Sorry, no match"
-
-        search = []
-
-        for question in questions:
-            if question['id'] == id:
-                search.append(question)
-        return jsonify(search)
-
-    @app.route('/api/v1/questions', methods=['POST'])
-    def add_question():
-        added_question = questions.append(request.get_json())
-        return jsonify(added_question)
-
-    @app.route('/api/v1/questions/id/answers', methods=['POST'])
-    def add_answer():
-        if 'id' in request.args:
-            id = int(request.args['id'])
-
-        answers = []
-
-        for question in questions:
-            if question['id'] == id:
-                added_answer = answers.append(request.get_json())
-                return jsonify(answers)
-            else:
-                return "No such question"
+            return "No such question"
 
 
 app.run(debug=True)
