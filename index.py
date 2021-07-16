552d013ea77f2d8f6dbc4d942d81e5b155abe060
Leoncenno <leonarddng@gmail.com>
2021-07-16 14:14:44 +0300

    get specific question end point

--- index.py
+++ index.py
@@ -4,16 +4,16 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 questions = [
-    {'id': 0,
+    {'pk': 0,
+     'question': 'mahsfhduujhws anshdshdhj akjsjdiinasksk nmjsbhhjjskknms  sbbhbhsjhjdsn',
+     'answers': [{'answer': 'none yet'}]},
+    {'pk': 1,
      'question': 'mahsfhduujhws anshdshdhj akjsjdiinasksk nmjsbhhjjskknms  sbbhbhsjhjdsn',
      'answers': [{'answer': ''}]},
-    {'id': 1,
-     'question': 'mahsfhduujhws anshdshdhj akjsjdiinasksk nmjsbhhjjskknms  sbbhbhsjhjdsn',
+    {'pk': 2,
+     'question': 'I leon the one making this ahppen at the firsty place?',
      'answers': [{'answer': ''}]},
-    {'id': 2,
-     'question': 'mahsfhduujhws anshdshdhj akjsjdiinasksk nmjsbhhjjskknms  sbbhbhsjhjdsn',
-     'answers': [{'answer': ''}]},
-    {'id': 3,
+    {'pk': 3,
      'question': 'mahsfhduujhws anshdshdhj akjsjdiinasksk nmjsbhhjjskknms  sbbhbhsjhjdsn',
      'answers': [{'answer': ''}]},
 ]
@@ -22,6 +22,15 @@ questions = [
 @app.route('/api/v1/questions')
 def all_questions():
     return jsonify(questions)
+
+
+@app.route('/api/v1/question/<pk>')
+def one_question(pk):
+    for question in questions:
+        if question['pk'] == int(pk):
+            return jsonify(question)
+        else:
+            return jsonify({'warning': 'Invalid question ID'}), 404
 
 
 app.run(debug=True)
